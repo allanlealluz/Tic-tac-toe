5fcc0d48f5cc5b7d4eb5db8879cf45e7374078b6
Allan <69430214+allanlealluz@users.noreply.github.com>
2023-08-05 20:45:08 -0300

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,43 +1,40 @@
-matriz = [[0,0,0],[0,0,0],[0,0,0]]
-acerto1 = 0
-colunas = []
+matriz = [[' ']*3 for _ in range(3)]
 ganhou = False
-while ganhou != True:
+
+while not ganhou:
     print('Jogador 1')
-    for valor in matriz:
-        print(f'{valor}')
-    linha = int(input('Escolha uma linha de 1 a 3'))
-    coluna = int(input('Escolha uma coluna de 1 a 3'))
+    
+    for linha in matriz:
+        print(' '.join(linha))
+        
+    linha = int(input('Escolha uma linha de 1 a 3: '))
+    coluna = int(input('Escolha uma coluna de 1 a 3: '))
+    
     matriz[linha-1][coluna-1] = 'x'
-    for valor in matriz:
-        print(f'{valor}')
-        if valor.count('x') == 3:
-          print('Ganhou')
-          ganhou = True
-    for i in range(0,3):
-        colunas.append(matriz[0][i])
-        colunas.append(matriz[1][i])
-        colunas.append(matriz[2][i])
-        if colunas.count('x') == 3:
-            print('ganhou')
+    
+    for linha in matriz:
+        if linha.count('x') == 3:
+            print('Ganhou')
             ganhou = True
-        else:
-           colunas.clear()
-    diagonal1 = []
-    diagonal1.append(matriz[0][0])
-    diagonal1.append(matriz[1][1])
-    diagonal1.append(matriz[2][2])
-    diagonal2 = []
-    diagonal2.append(matriz[0][2])
-    diagonal2.append(matriz[1][1])
-    diagonal2.append(matriz[2][0])
+            break
+    
+    for i in range(3):
+        coluna = [matriz[j][i] for j in range(3)]
+        if coluna.count('x') == 3:
+            print('Ganhou')
+            ganhou = True
+            break
+    
+    diagonal1 = [matriz[i][i] for i in range(3)]
+    diagonal2 = [matriz[i][2-i] for i in range(3)]
+    
     if diagonal1.count('x') == 3 or diagonal2.count('x') == 3:
-        print('ganhou')
+        print('Ganhou')
         ganhou = True
-    diagonal1.clear()
-    diagonal2.clear()
-    if ganhou == True:
+        
+    if ganhou:
         break
+    
     print('-'*30)
     print('Jogador 2')
     for valor in matriz:
